9135cc438fed3003fbb9bcfcec3e44ecc1a730fa
JATINDER JOHAL <johaljatinder39@gmail.com>
2017-01-23 18:22:14 -0800

    Update assign3.js

--- assign3.js
+++ assign3.js
@@ -4,13 +4,13 @@ let myArr1 = []
 myArr1.push(myVal[2])
 myArr1.push(myVal[3])
 
-let mySum = Number(myVal[2]) + Number(myVal[3])
+let Sum = Number(myVal[2]) + Number(myVal[3])
 
-if(mySum > 10 && mySum < 100)
+if(Sum > 10 && Sum < 100)
 {
     console.log("Total of two numbers is greater than 10 but less than 100")
 } 
-else if(mySum > 100 && mySum < 1000)
+else if(Sum > 100 && Sum < 1000)
 {
     console.log("Total of two numbers is greater than 100 but less than 1000")
 } 
